1d6a467e159a9711a9124b47bd9596ef4d1df305
Indigo Lily <yulex.42@gmail.com>
2017-06-28 17:56:30 -0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function circle() {
+  this.pos = createVector( random(width), random(height) );
+  this.isWhite = (Math.random() < .5);
+  this.diameter = 1/random(1,2);
+}
+
+circle.prototype.draw = function() {
+  this.diameter *= 1.01;
+  noStroke();
+  fill( this.isWhite ? 255 : 0 );
+  ellipse( this.pos.x, this.pos.y, this.diameter, this.diameter );
+}
+
+var circles = [];
+
+function setup() {
+  createCanvas( windowWidth, windowHeight );
+  background(127);
+}
+
+function draw() {
+  if( frameCount % 6 === 0 ) {
+    circles.push( new circle() );
+  }
+  var flags = [];
+  
+  // Draw circles
+  for( let i = 0; i < circles.length; i++ ) {
+    circles[i].draw();
+    
+    // Flag circles for removal
+    if( circles[i].diameter / 2 > max( width, height ) ) {
+      flags.push(i);
+    }
+  }
+  
+  // Remove circles
+  for( let i = flags.length - 1; i >= 0; i-- ) {
+    circles.splice(i,1);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+}
+
+// Create circle object that grows until a certain diameter and then is removed
